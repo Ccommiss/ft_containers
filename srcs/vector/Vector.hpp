334706a8404e5686cf9ea529e23ead655b33853f
ccommiss <ccommiss@student.42.fr>
2022-03-28 12:34:22 +0200

    relationnal ops have an invalid read

--- srcs/vector/Vector.hpp
+++ srcs/vector/Vector.hpp
@@ -730,7 +730,7 @@ namespace ft
 	template <class T, class Alloc>
 	bool operator<(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs)
 	{
-		return (ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin()));
+		return (ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
 	}
 
 /*
--- srcs/vector/relationnal_operators_tools.hpp
+++ srcs/vector/relationnal_operators_tools.hpp
@@ -39,9 +39,10 @@ template <class InputIterator1, class InputIterator2>
 bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
 {
 	while (first1 != last1) {
-		if (!(*first1 == *first2))   // or: if (!pred(*first1,*first2)), for version 2
+		if (!(*first1 == *first2)) 
 			return false;
-		++first1; ++first2;
+		++first1;
+		++first2;
 	}
 	return true;
 }
